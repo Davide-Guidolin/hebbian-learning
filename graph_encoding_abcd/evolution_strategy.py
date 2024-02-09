Neuron-centric ABCD and weight masking fix

graph_encoding_abcd/evolution_strategy.py
@@ -30,7 +30,7 @@ def compute_centered_ranks(x):
   return y
 
 class EvolutionStrategy:
-    def __init__(self, rolled_model, dataset_type="CIFAR10", population_size=100, num_threads=1, sigma=0.1, abcd_perturbation_std=1, abcd_learning_rate=0.2, abcd_lr_decay=0.995, bp_last_layer=True, bp_learning_rate=0.01):
+    def __init__(self, rolled_model, dataset_type="CIFAR10", population_size=100, num_threads=1, sigma=0.1, abcd_perturbation_std=1, abcd_learning_rate=0.02, abcd_lr_decay=0.995, bp_last_layer=True, bp_learning_rate=0.01):
         self.model = rolled_model
         
         self.population_size = population_size
@@ -50,9 +50,26 @@ class EvolutionStrategy:
         self.unrolled_model = UnrolledModel(self.model, self.input_size)
         self.params = self.init_ABCD_parameters(self.unrolled_model.get_new_model())
         
+        print(f"ABCD parameters initialized: {self.get_ABCD_params_number()} parameters ({getsizeof(self.params) / 1024 ** 2:.5f} MB)")
+        
         self.bp_last_layer = bp_last_layer
         self.bp_lr = bp_learning_rate      
         
+        
+    def get_ABCD_params_number(self, print_layers=False):
+        pms = 0
+        l = 0
+        for layer in self.params:
+            for side in ['in', 'out']:
+                for p in self.params[layer][side]:
+                    if self.params[layer][side][p] != None:
+                        if print_layers:
+                            print(f"{l}_{side} - {p}: {self.params[layer][side][p].shape}")
+                        pms += self.params[layer][side][p].shape[0]
+            l += 1
+        
+        return pms        
+
     def init_ABCD_parameters(self, net: nn.Sequential, start_idx: int = 0, end_idx: int = -1, device: str = 'cpu') -> dict:
         if end_idx == -1 or end_idx > len(net):
             end_idx = len(net)
@@ -62,21 +79,66 @@ class EvolutionStrategy:
         i = start_idx
         while i < end_idx:
             if type(net[i]) == nn.Linear:
-                params[l_index] = {'A':None, 'B': None, 'C':None, 'D': None}
+                params[l_index] = {'in': None, 'out': None}
+                params[l_index]['in'] = {'A':None, 'B': None, 'C':None, 'D': None}
+                params[l_index]['out'] = {'A':None, 'B': None, 'C':None, 'D': None}
                 l_index += 1
             i += 1
-        params[l_index] = {'A':None, 'B': None, 'C':None, 'D': None}
         
+        last_l_index = l_index - 1
         l_index = start_idx
         i = start_idx
+        after_pooling = False
         while i < end_idx:
+            if type(net[i]) in [nn.MaxPool2d, nn.AvgPool2d]:
+                after_pooling = True
             if type(net[i]) == nn.Linear:
                 layer = net[i]
 
-                params[l_index]['A'] = torch.randn(layer.weight.shape[1], device=device)
-                params[l_index + 1]['B'] = torch.randn(layer.weight.shape[0], device=device)
-                params[l_index]['C'] = torch.randn((layer.weight.shape[0], layer.weight.shape[1]), device=device)
-                params[l_index]['D'] = torch.randn((layer.weight.shape[0], layer.weight.shape[1]), device=device)
+                in_shape = layer.weight.shape[1]
+                out_shape = layer.weight.shape[0]
+                
+                # first linear no B in
+                if l_index == 0:
+                    params[l_index]['in']['A'] = torch.randn(in_shape, device=device)
+                    params[l_index]['in']['C'] = torch.randn(in_shape, device=device)
+                    params[l_index]['in']['D'] = torch.randn(in_shape, device=device)
+                    
+                    params[l_index]['out']['A'] = torch.randn(out_shape, device=device)
+                    params[l_index]['out']['B'] = torch.randn(out_shape, device=device)
+                    params[l_index]['out']['C'] = torch.randn(out_shape, device=device)
+                    params[l_index]['out']['D'] = torch.randn(out_shape, device=device)
+                # last linear no A out
+                elif l_index == last_l_index:
+                    if after_pooling:
+                        params[l_index]['in']['A'] = torch.randn(in_shape, device=device)
+                        params[l_index]['in']['C'] = torch.randn(in_shape, device=device)
+                        params[l_index]['in']['D'] = torch.randn(in_shape, device=device)
+                        
+                        params[l_index - 1]['out']['A'] = None # not needed A before pooling as layers do not share neurons
+                        
+                        after_pooling = False
+                        
+                    params[l_index]['out']['B'] = torch.randn(out_shape, device=device)
+                    params[l_index]['out']['C'] = torch.randn(out_shape, device=device)
+                    params[l_index]['out']['D'] = torch.randn(out_shape, device=device)
+                    
+                else:
+                    if after_pooling:
+                        params[l_index]['in']['A'] = torch.randn(in_shape, device=device)
+                        params[l_index]['in']['C'] = torch.randn(in_shape, device=device)
+                        params[l_index]['in']['D'] = torch.randn(in_shape, device=device)
+                        
+                        params[l_index - 1]['out']['A'] = None # not needed A before pooling as layers do not share neurons
+                        
+                        after_pooling = False
+                    
+                    params[l_index]['out']['A'] = torch.randn(out_shape, device=device)
+                    params[l_index]['out']['B'] = torch.randn(out_shape, device=device)
+                    params[l_index]['out']['C'] = torch.randn(out_shape, device=device)
+                    params[l_index]['out']['D'] = torch.randn(out_shape, device=device)
+                    
+                
                 l_index += 1
             i += 1
         
@@ -86,14 +148,11 @@ class EvolutionStrategy:
     def perturbate(self, params):
         new_p = deepcopy(params)
         for layer in new_p:
-            for p in new_p[layer]:
-                if layer == 0 and p == 'B':
-                    continue
-                if layer == list(new_p.keys())[-1] and p != 'B':
-                    continue
-                
-                noise = torch.randn_like(new_p[layer][p])
-                new_p[layer][p].add_(self.perturbation_factor * noise)
+            for side in ['in', 'out']:
+                for p in new_p[layer][side]:
+                    if new_p[layer][side][p] != None:
+                        noise = torch.randn_like(new_p[layer][side][p])
+                        new_p[layer][side][p].add_(self.perturbation_factor * noise)
         
         return new_p
     
@@ -229,15 +288,13 @@ class EvolutionStrategy:
             
             self.update_factor = self.abcd_lr / (self.population_size * self.sigma)
             for layer in population[0].keys():
-                for key in population[0][layer].keys():
-                    if layer == 0 and key == 'B':
-                        continue
-                    if layer == list(population[0].keys())[-1] and key != 'B':
-                        continue
+                for side in population[0][layer].keys():
+                    for key in population[0][layer][side].keys():
+                        if self.params[layer][side][key] != None:
                     
-                    param_pop = np.array([p[layer][key] for p in population])
+                            param_pop = np.array([p[layer][side][key] for p in population])
                     
-                    self.params[layer][key].add_(torch.from_numpy(self.update_factor * np.dot(param_pop.T, ranks).T))
+                            self.params[layer][side][key].add_(torch.from_numpy(self.update_factor * np.dot(param_pop.T, ranks).T))
             
         if self.abcd_lr > 0.001:
             self.abcd_lr *= self.decay
@@ -250,7 +307,7 @@ class EvolutionStrategy:
     
     
     def run(self, iterations):
-        keep_best_only = True
+        keep_best_only = False
         if keep_best_only:
             self.best_total_score = 0
         

graph_encoding_abcd/hebbian.py
@@ -4,30 +4,34 @@ from copy import deepcopy
 from torch.profiler import profile, record_function, ProfilerActivity
 import os, psutil
 
-# Linear (3072 -> 6272)
-# ~ 3  s no compiled
-# ~ 88 ms compiled
+# Linear (12288 -> 23064)
+# ~ 741 ms no compiled
+# ~ 57 ms compiled
 @torch.compile()
-def abcd(pre, post, a, b, c, d):
-    # return (a*pre)[:, None, :] + (b*post)[:, :, None] + c*pre[:, None, :]*post[:, :, None] + d
-    s1 = pre.shape[0]
-    s2 = post.shape[-1]
-    s3 = pre.shape[-1]
+def abcd(pre, post, a, b, c0, c1, d0, d1):
+    
+    s1 = pre.shape[0] # batch size
+    s2 = post.shape[-1] # out_shape
+    s3 = pre.shape[-1] # in_shape
 
     pre = pre.unsqueeze(1).expand(-1, s2, -1)
-    
+    c0 = c0.unsqueeze(0).unsqueeze(1).expand(-1, s2, -1)
+    d0 = d0.unsqueeze(0).expand(s2, -1)
 
     post = post.unsqueeze(-1).expand(-1, -1, s3)
+    c1 = c1.unsqueeze(0).unsqueeze(-1).expand(-1, -1, s3)
+    d1 = d1.unsqueeze(-1).expand(-1, s3)
     
-    result = torch.zeros(s1, s2, s3)
+    d = (d0 * d1).unsqueeze(0).expand(s1, -1, -1)
+    result = torch.zeros(s1, s2, s3).add_(d)
     
     result.addcmul_(a.expand(s1, s2, -1), pre)
 
     result.addcmul_(b.unsqueeze(-1).expand(s1, -1, s3), post)
 
-    result.addcmul_(c.unsqueeze(0).expand(s1, -1, -1), pre * post)
+    result.addcmul_(c0 * c1, pre * post)
 
-    return torch.mean(result, dim=0).add_(d)
+    return torch.mean(result, dim=0)
 
 
 def shared_weights(layer, w_matrix):
@@ -52,13 +56,26 @@ def shared_weights(layer, w_matrix):
 
 def update_weights(layer, input, output, ABCD_params, lr=0.0001, shared_w=False):
 
-    A = ABCD_params[layer.idx]['A']
-    B = ABCD_params[layer.idx + 1]['B']
-    C = ABCD_params[layer.idx]['C']
-    D = ABCD_params[layer.idx]['D']
-    
-    w_matrix = abcd(input, output, A, B, C, D)
+    if ABCD_params[layer.idx]['in']['C'] == None: # ABCD schared with previous layer
+        A = ABCD_params[layer.idx - 1]['out']['A']
+        B = ABCD_params[layer.idx]['out']['B']
+        C0 = ABCD_params[layer.idx - 1]['out']['C']
+        C1 = ABCD_params[layer.idx]['out']['C']
+        D0 = ABCD_params[layer.idx - 1]['out']['D']
+        D1 = ABCD_params[layer.idx]['out']['D']
+    else:
+        A = ABCD_params[layer.idx]['in']['A']
+        B = ABCD_params[layer.idx]['out']['B']
+        C0 = ABCD_params[layer.idx]['in']['C']
+        C1 = ABCD_params[layer.idx]['out']['C']
+        D0 = ABCD_params[layer.idx]['in']['D']
+        D1 = ABCD_params[layer.idx]['out']['D']
 
+    w_matrix = abcd(input, output, A, B, C0, C1, D0, D1)
+    
+    if hasattr(layer, 'mask_tensor'):
+        w_matrix.mul_(layer.mask_tensor.t())
+    
     if shared_w:
         w_matrix = shared_weights(layer, w_matrix)
 
@@ -69,8 +86,10 @@ def update_weights(layer, input, output, ABCD_params, lr=0.0001, shared_w=False)
 
 @torch.compile()
 def softhebb(x, pre_act, w):
-    soft = torch.softmax(pre_act, dim=-1)
+    soft = - torch.softmax(pre_act, dim=-1)
     max_neuron = torch.argmax(soft, dim=-1)
+    # anti-hebbian
+    soft[:, max_neuron] = -soft[:, max_neuron]
     
     dw = torch.matmul(soft.t(), x)
     yu = torch.multiply(soft, pre_act)
@@ -86,12 +105,15 @@ def softhebb_update(layer, x, pre_act, lr=0.0001, shared_w=False):
     
     w_update, max_neuron = softhebb(x, pre_act, layer.weight)
     
-    #normalize
+    # normalize
     w_update = w_update / torch.abs(w_update).amax()
 
     w_matrix = layer.weight + lr * w_update
     
+    if hasattr(layer, 'mask_tensor'):
+        w_matrix.mul_(layer.mask_tensor.t())
+    
     if shared_w:
         w_matrix = shared_weights(layer, w_matrix)
-            
+
     layer.weight = nn.Parameter(w_matrix, requires_grad=False)

graph_encoding_abcd/unrolled_model.py
@@ -97,11 +97,10 @@ class UnrolledModel:
         
         print(f"MEM after mask_t: {psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2:.2f} MB")
         print("Multiply weights")
-        
+        print("NON ZERO params", torch.count_nonzero(mask_tensor.to_dense()))
         layer.weight.data.mul_(mask_tensor.t())
         
-        del mask_tensor
-        
+        layer.mask_tensor = mask_tensor
         layer.shared_weights = shared_w
         
         return layer